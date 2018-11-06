28b96fa2e438c53f9a2e76e7488c036641a16330
Alexander Wolff <alex.wolff@jeloexuma.com>
2018-11-06 21:05:31 +1000

    improvements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Highcharts from 'highcharts/highstock'
 import StockChart from './components/StockChart'
 import Paper from '@material-ui/core/Paper';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Table from '@material-ui/core/Table';
@@ -18,184 +17,209 @@ import './App.css';
 
 
 let id = 0;
+
 function createData(name, wkn, current, chartUrl, symbol) {
-  id += 1;
-  return { id, name, wkn, current, chartUrl, symbol };
+    id += 1;
+    return {id, name, wkn, current, chartUrl, symbol};
 }
 
 // Create a table
 const rows = [
-  createData('Apple', '865985', 270.17, 'https://x.onvista.de/typ1.chart?ID_NOTATION=9385885&LEGEND=0&FROM=20.09.2018&TO=05.11.2018&colPriceLine=00519e&gfxtools_customer=onvista', 'AAPL'),
-  createData('Facebook', 'A1JWVX', 129.61, 'https://x.onvista.de/typ1.chart?ID_NOTATION=9385885&LEGEND=0&FROM=20.09.2018&TO=05.11.2018&colPriceLine=00519e&gfxtools_customer=onvista', 'FB')
+    createData('Apple', '865985', 270.17, 'https://x.onvista.de/typ1.chart?ID_NOTATION=9385885&LEGEND=0&FROM=20.09.2018&TO=05.11.2018&colPriceLine=00519e&gfxtools_customer=onvista', 'AAPL'),
+    createData('Facebook', 'A1JWVX', 129.61, 'https://x.onvista.de/typ1.chart?ID_NOTATION=9385885&LEGEND=0&FROM=20.09.2018&TO=05.11.2018&colPriceLine=00519e&gfxtools_customer=onvista', 'FB')
 ];
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: false,
-      showGraph: false,
-      currentGraph: null, // Maybe usefull for the clicked row
-      error: 0,
-    }
-    this.handleMiniGraphClick = this.handleMiniGraphClick.bind(this); // Maybe usefull for the clicked row
-  }
-  handleClickAway = (e) => {
-    console.log(e.target.className);
-    if (e.target.className === 'chart-mini-image') {
-      return;
-    }
-    this.setState({
-      showGraph: false,
-    });
-  };
-  handleMiniGraphClick(e, row) {
-    // Only  Show Grpah wenn not cllickt on the mini graph and if it is open
-    // then close graph
-    if (this.state.showGraph === true && e.target.className === 'chart-mini-image') {
-      this.setState({
-        showGraph: false,
-      });
-      return;
-    }
-
-    let symbol = row.symbol;
-
-    this.setState({isLoading: true});
-    axios.get(`https://api.iextrading.com/1.0/stock/${symbol}/chart/1y`)
-      .then(res => {
-        const stockData = res.data;
-
-        let parsedData = stockData.map((data) => {
-          return [dayjs(data.date).valueOf(), data.open, data.high, data.low, data.close]
-        })
-
-        let parsedVol = stockData.map((data) => {
-          return [dayjs(data.date).valueOf(), data.volume]
-        })
-
-        this.setState({
-          showGraph: true,
-          isLoading: false,
-          chartOptions: {
-            xAxis: false,
-            yAxis: [{
-              labels: {
-                align: 'right',
-                x: -3
-              },
-              title: {
-                text: 'OHLC'
-              },
-              height: '60%',
-              lineWidth: 2,
-              resize: {
-                enabled: true
-              }
-            }, {
-              labels: {
-                align: 'right',
-                x: -3
-              },
-              title: {
-                text: 'Volume'
-              },
-              top: '65%',
-              height: '35%',
-              offset: 0,
-              lineWidth: 2
-            }],
-            tooltip: {
-              split: true
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false,
+            showGraph: null,
+            chartStyle: {
+                display: 'none'
             },
-            title: {
-              text: 'Apple'
-            },
-            series: [
-              {
-                name: 'Apple',
-                data: parsedData,
+            currentGraph: null, // Maybe usefull for the clicked row
+            error: 0,
+            chartsOptions: {
+                xAxis: false,
+                yAxis: [{
+                    labels: {
+                        align: 'right',
+                        x: -3
+                    },
+                    title: {
+                        text: 'OHLC'
+                    },
+                    height: '60%',
+                    lineWidth: 2,
+                    resize: {
+                        enabled: true
+                    }
+                }, {
+                    labels: {
+                        align: 'right',
+                        x: -3
+                    },
+                    title: {
+                        text: 'Volume'
+                    },
+                    top: '65%',
+                    height: '35%',
+                    offset: 0,
+                    lineWidth: 2
+                }],
                 tooltip: {
-                  valueDecimals: 2
+                    split: true
                 },
-              }, {
-                type: 'column',
-                name: 'Volume',
-                data: parsedVol,
-                yAxis: 1
-              }
-            ]
-          }
-        });
-      })
-  }
+                series: null
+            }
+        }
+        this.handleMiniGraphClick = this.handleMiniGraphClick.bind(this); // Maybe usefull for the clicked row
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <AppBar position="static">
-          <Typography variant="h2" color="inherit">
-            Musterdepot
-          </Typography>
-          <Typography variant="h6" color="inherit">
-            Inline-Chart Demo (Reactjs)
-          </Typography>
-        </AppBar>
-        <div className="content">
-          <Paper>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>WKN</TableCell>
-                  <TableCell>Aktueller Kurs</TableCell>
-                  <TableCell>Chart</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map(row => {
-                  return (
-                    <TableRow key={row.id}>
-                      <TableCell component="th" scope="row">
-                        {row.name}
-                      </TableCell>
-                      <TableCell>{row.wkn}</TableCell>
-                      <TableCell>{row.current} $</TableCell>
-                      <TableCell>
-                        {this.state.showGraph ? (
-                          <div className="popup-graph">
-                            <ClickAwayListener onClickAway={this.handleClickAway}>
-                              <Paper elevation={3}>
-                                <StockChart
-                                  key={row.id}
-                                  highcharts={Highcharts}
-                                  options={this.state.chartOptions} />
-                              </Paper>
-                            </ClickAwayListener>
-                          </div>
-                        ) : ('')}
-                        {this.state.isLoading ? (
-                          <div className="chart-icon">
-                            <CircularProgress key={row.id} className="loader" size={30} />
-                          </div>
-                        ) : (
-                            <img
-                              className="chart-mini-image"
-                              key={row.id}
-                              onClick={(e) => this.handleMiniGraphClick(e, row)}
-                              src={row.chartUrl} alt="logo" />
-                        )}
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </Paper>
-        </div>
-      </div>
-    );
-  }
+    handleClickAway = (e) => {
+        this.setState({
+            chartsOptions: {
+                ...this.state.chartsOptions,
+                series: null,
+            },
+            chartStyle: {
+                display: 'none'
+            },
+            showGraph:null
+        });
+    };
+
+    offset(el) {
+        var rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+
+    handleMiniGraphClick(e, row) {
+        // Only  Show Grpah wenn not cllickt on the mini graph and if it is open
+        // then close graph
+        if (this.state.showGraph === row.id) {
+            this.setState({
+                showGraph: null,
+                chartsOptions: {
+                    ...this.state.chartsOptions,
+                    series: null
+                }
+            });
+            return;
+        }
+
+        console.log(e.target);
+        this.setState({
+            isLoading: row.id, showGraph: row.id,
+            chartStyle: {
+                display: 'block',
+                top: this.offset(e.target).top,
+                left: this.offset(e.target).left - 400,
+            },
+            chartsOptions: {
+                ...this.state.chartsOptions,
+                series: [],
+                title: {
+                    text: row.name,
+                },
+            }
+        }, () => {
+            axios.get(`https://api.iextrading.com/1.0/stock/${row.symbol}/chart/1y`)
+                .then(res => {
+                    this.setState({
+                        showGraph: row.id,
+                        isLoading: false,
+                        chartsOptions: {
+                            ...this.state.chartsOptions,
+                            title: {
+                                text: row.name,
+                            },
+                            series: [
+                                {
+                                    name: row.name,
+                                    data: res.data.map((data) => {
+                                        return [dayjs(data.date).valueOf(), data.open, data.high, data.low, data.close]
+                                    }),
+                                    tooltip: {
+                                        valueDecimals: 2
+                                    },
+                                }, {
+                                    type: 'column',
+                                    name: 'Volume',
+                                    data: res.data.map((data) => {
+                                        return [dayjs(data.date).valueOf(), data.volume]
+                                    }),
+                                    yAxis: 1
+                                }
+                            ]
+                        }
+                    });
+                })
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="popup-graph" style={this.state.chartStyle}>
+                    <ClickAwayListener onClickAway={this.handleClickAway}>
+                        <Paper elevation={3}>
+                            <StockChart
+                                loading={!!this.state.isLoading}
+                                highcharts={Highcharts}
+                                options={this.state.chartsOptions}/>
+                        </Paper>
+                    </ClickAwayListener>
+                </div>
+                <AppBar position="static">
+                    <Typography variant="h2" color="inherit">
+                        Musterdepot
+                    </Typography>
+                    <Typography variant="h6" color="inherit">
+                        Inline-Chart Demo (Reactjs)
+                    </Typography>
+                </AppBar>
+                <div className="content">
+                    <Paper>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>WKN</TableCell>
+                                    <TableCell>Aktueller Kurs</TableCell>
+                                    <TableCell>Chart</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map(row => {
+                                    return (
+                                        <TableRow key={row.id}>
+                                            <TableCell component="th" scope="row">
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell>{row.wkn}</TableCell>
+                                            <TableCell>{row.current} $</TableCell>
+                                            <TableCell>
+                                                <img
+                                                    className="chart-mini-image"
+                                                    key={row.id}
+                                                    onClick={(e) => this.handleMiniGraphClick(e, row)}
+                                                    src={row.chartUrl} alt="logo"/>
+
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            </TableBody>
+                        </Table>
+                    </Paper>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/StockChart.js
+++ src/components/StockChart.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react';
+
 import HighchartsReact from 'highcharts-react-official'
 
-const StockChart = ({ options, highcharts }) => <HighchartsReact
-    highcharts={highcharts}
-    constructorType={'stockChart'}
-    options={options}
-/>
+export default class StockChart extends Component {
 
-export default StockChart
+    constructor(props) {
+        super(props)
+        console.log(props);
+
+        if(this.chart) {
+            this.chart.chart.showLoading()
+        }
+        if (props.loading && this.chart) {
+            this.chart.chart.showLoading()
+        }
+        if (!props.loading && this.chart) {
+            this.chart.chart.hideLoading()
+            this.chart.chart.redraw();
+        }
+    }
+
+    componentWillReceiveProps(newProps) {
+        if(this.chart) {
+            this.chart.chart.showLoading()
+        }
+        if (newProps.loading && this.chart) {
+            this.chart.chart.showLoading()
+        }
+        if (!newProps.loading && this.chart) {
+            this.chart.chart.hideLoading()
+            this.chart.chart.redraw();
+        }
+    }
+
+    render() {
+        let {options, highcharts} = this.props;
+        return <HighchartsReact
+            ref={(chart) => this.chart = chart}
+            highcharts={highcharts}
+            constructorType={'stockChart'}
+            options={options}
+        />
+    }
+}
