0f72b061ec97a07f0180391bafe965b3a8c0257c
Alexander Wolff <alex.wolff@jeloexuma.com>
2018-11-08 08:00:21 +1000

    refactor

--- src/components/StockChart.js
+++ src/components/StockChart.js
@@ -1,34 +1,157 @@
 import React, {Component} from 'react';
-
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import HighchartsReact from 'highcharts-react-official'
+import Highcharts from 'highcharts/highstock'
+import axios from 'axios';
+import dayjs from 'dayjs'
+import Paper from '@material-ui/core/Paper';
 
 export default class StockChart extends Component {
 
     constructor(props) {
         super(props)
-        console.log(props);
+
+        this.state = {
+            isLoading: true,
+            showGraph: false,
+            chartStyle: {
+                display: 'none'
+            },
+            chartsOptions: {
+                xAxis: false,
+                yAxis: [{
+                    labels: {
+                        align: 'right',
+                        x: -3
+                    },
+                    title: {
+                        text: 'OHLC'
+                    },
+                    height: '60%',
+                    lineWidth: 2,
+                    resize: {
+                        enabled: true
+                    }
+                }, {
+                    labels: {
+                        align: 'right',
+                        x: -3
+                    },
+                    title: {
+                        text: 'Volume'
+                    },
+                    top: '65%',
+                    height: '35%',
+                    offset: 0,
+                    lineWidth: 2
+                }],
+                tooltip: {
+                    split: true
+                },
+                series: null
+            }
+        }
     }
 
-    componentWillReceiveProps(newProps) {
-        if(this.chart) {
-            this.chart.chart.showLoading()
+    handleClickAway = (e) => {
+        this.setState({
+            chartsOptions: {
+                ...this.state.chartsOptions,
+                series: null,
+            },
+            chartStyle: {
+                display: 'none'
+            },
+            showGraph: null
+        });
+    };
+
+    handleMiniGraphClick(e) {
+        let {stock} = this.props;
+        // Only  Show Grpah wenn not cllickt on the mini graph and if it is open
+        // then close graph
+        if (this.state.showGraph === stock.id) {
+            this.setState({
+                showGraph: null,
+                chartsOptions: {
+                    ...this.state.chartsOptions,
+                    series: null
+                }
+            });
+            return;
         }
-        if (newProps.loading && this.chart) {
-            this.chart.chart.showLoading()
-        }
-        if (!newProps.loading && this.chart) {
-            this.chart.chart.hideLoading()
-            this.chart.chart.redraw();
-        }
+
+        this.setState({
+            isLoading: true,
+            chartStyle: {
+                display: 'flex',
+            }
+        });
+
+        axios.get(`https://api.iextrading.com/1.0/stock/${stock.symbol}/chart/1y`)
+            .then(res => {
+                this.setState({
+                    showGraph: true,
+                    isLoading: false,
+                    chartStyle: {
+                        ...this.state.chartStyle,
+                        display: 'block',
+                    },
+                    chartsOptions: {
+                        ...this.state.chartsOptions,
+                        title: {
+                            text: stock.name,
+                        },
+                        series: [
+                            {
+                                name: stock.name,
+                                data: res.data.map((data) => {
+                                    return [dayjs(data.date).valueOf(), data.open, data.high, data.low, data.close]
+                                }),
+                                tooltip: {
+                                    valueDecimals: 2
+                                },
+                            }, {
+                                type: 'column',
+                                name: 'Volume',
+                                data: res.data.map((data) => {
+                                    return [dayjs(data.date).valueOf(), data.volume]
+                                }),
+                                yAxis: 1
+                            }
+                        ]
+                    }
+                });
+            })
     }
 
     render() {
-        let {options, highcharts} = this.props;
-        return <HighchartsReact
-            ref={(chart) => this.chart = chart}
-            highcharts={highcharts}
-            constructorType={'stockChart'}
-            options={options}
-        />
+        return <div style={{position: 'relative'}}>
+            <img
+                className="chart-mini-image"
+                onClick={this.handleMiniGraphClick.bind(this)}
+                src={this.props.stock.chartUrl} alt="logo"/>
+
+            {this.state.isLoading || this.state.showGraph ?
+                <Paper className="popup-graph" style={this.state.chartStyle} elevation={3}>
+                    <ClickAwayListener onClickAway={this.handleClickAway}>
+                        {this.state.isLoading ?
+                            <div className="chart-icon">
+                                <CircularProgress className="loader" size={30}/>
+                            </div>
+                            :
+                            <div><HighchartsReact
+                                ref={(chart) => this.chart = chart}
+                                highcharts={Highcharts}
+                                constructorType={'stockChart'}
+                                options={this.state.chartsOptions}
+                            />
+                            </div>
+                        }
+                    </ClickAwayListener>
+                </Paper> : null
+            }
+        </div>
     }
 }
